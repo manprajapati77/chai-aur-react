5e3a1b055e7635d469b623410930cf94899d34c2
manprajapatie <manprajapatie@gmail.com>
2024-08-08 23:04:15 +0530

    Learn about function name

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+   return (
+    <>
+   <h1>Chai aur React with Viet | Man </h1>
+   <p>we can't return more than 1 element in jsx when we wrap element in empty tag this is called fragment</p>
+   </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.js
@@ -0,0 +1,9 @@
+function chai(){
+    return(
+        <h3>
+            Chai H3
+        </h3>
+    )
+}
+
+export default chai
