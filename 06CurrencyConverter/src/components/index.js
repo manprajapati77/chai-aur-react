57e38858015c729ef026c067fa6d53f19cab92dc
manprajapatie <manprajapatie@gmail.com>
2024-09-24 21:13:49 +0530

    nahi ho pa raha he

---
+++ 06CurrencyConverter/src/components/index.js
@@ -0,0 +1,4 @@
+//when we have lots of component then we have to export each and have to access each in app jsx file to prevent this we made a file index.js and import all component in it and then export only one file 
+import InputBox from "./InputBox"
+
+export {InputBox}
